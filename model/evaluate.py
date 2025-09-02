f400e29b7f3cb937aa1b997b77f45f770f766e58
Isha Chauhan <trynafind.karma@gmsil.com>
2025-09-02 10:49:34 +0530

    second command 1.1

--- model/evaluate.py
+++ model/evaluate.py
@@ -13,6 +13,7 @@ y = data['species']
 
 # Split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # Load the saved model
 model = joblib.load('model/iris_model.pkl')
 
